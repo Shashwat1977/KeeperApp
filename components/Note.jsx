5faab15f90cf497b9d4bb424ce25df7743820473
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-04-16 12:07:29 +0530

    Initial commit

---
+++ components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>NOTE</h1>
+      <p>
+        Dummy para text to show that i dont know anything that i am doing
+        forgive me plz.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
